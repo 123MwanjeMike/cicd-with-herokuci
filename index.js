761af37a6b01efc4fe086740922ce864b94cf6be
Innocent Onyemaenu <innocent.onyemaenu@lixy.tech>
2022-03-04 13:09:17 +0100

    Factorial service

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Factorial calculator 🎊' });
 });
 
+app.get('/docs', (req, res) => {
+  const { host } = req.headers;
+  res.status(200).json({
+    message: 'Documentation',
+    request: `http://${host}/factorial/<number>`,
+    response: 'The factorial of <number> is <result>',
+    example: {
+      request: `http://${host}/factorial/5`,
+      response: 'The factorial of 5 is 120',
+    }
+  })
+})
+
 app.get('*', (req, res) => {
   res.status(404).json({ message: 'Resource not found.' });
 });
